26d42222150e7c014781de02e3688b2bca24f7e0
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-16 00:09:22 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { AITutor } from './components/AITutor';
 import { Features } from './components/Features';
-import { CourseLevels } from './components/CourseLevels';
+import { AITutor } from './components/AITutor';
 import { ContactBar } from './components/ContactBar';
-import { AdPromotion } from './components/AdPromotion';
-import { SocialMediaKit } from './components/SocialMediaKit';
-import { SourceExplorer } from './components/SourceExplorer';
 
-const App: React.FC = () => {
-  useEffect(() => {
-    const loader = document.getElementById('loading-screen');
-    if (loader) {
-      setTimeout(() => {
-        loader.style.opacity = '0';
-        setTimeout(() => loader.style.display = 'none', 1000);
-      }, 1500);
-    }
-  }, []);
-
+export default function App() {
   return (
-    <div className="min-h-screen bg-[#020617] text-white selection:bg-elite-gold/30 overflow-x-hidden" dir="rtl">
+    <div className="bg-[#020617] min-h-screen text-white" dir="rtl">
       <Navbar />
-      <main className="relative">
-        <Hero />
-        <div className="max-w-7xl mx-auto px-6 space-y-48 pb-40">
-          <section id="features"><Features /></section>
-          <section id="levels"><CourseLevels /></section>
-          <section id="tutor"><AITutor /></section>
-          <AdPromotion />
-          <SocialMediaKit />
-        </div>
-      </main>
+      <Hero />
+      <Features />
+      <AITutor />
       <ContactBar />
-      <SourceExplorer />
     </div>
   );
-};
-export default App;
+}

2af18f7351bd3439fd5c9771b2adedc1fcb8b62a
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-16 01:03:25 +0200

    Refactor App component and add loading screen effect
    Refactor App component to use arrow function and add loading screen effect.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { AITutor } from './components/AITutor';
 import { Features } from './components/Features';
+import { CourseLevels } from './components/CourseLevels';
+import { ContactBar } from './components/ContactBar';
+import { AdPromotion } from './components/AdPromotion';
+import { SourceExplorer } from './components/SourceExplorer';
 
-export default function App() {
+const App = () => {
+  useEffect(() => {
+    const loader = document.getElementById('loading-screen');
+    setTimeout(() => { if(loader) loader.style.display = 'none'; }, 2000);
+  }, []);
+
   return (
-    <div className="bg-[#020617] min-h-screen text-white">
+    <div className="bg-[#020617] text-white" dir="rtl">
       <Navbar />
       <Hero />
       <Features />
+      <CourseLevels />
       <AITutor />
+      <AdPromotion />
+      <ContactBar />
+      <SourceExplorer />
     </div>
   );
-}
+};
+export default App;

5a1e6523b6fb4ce08650d4f5aed7fb9a8a411bed
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-16 00:45:46 +0200

    Remove ContactBar component from App
    Removed ContactBar component from App and adjusted import statements.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { Features } from './components/Features';
 import { AITutor } from './components/AITutor';
-import { ContactBar } from './components/ContactBar';
+import { Features } from './components/Features';
 
 export default function App() {
   return (
-    <div className="bg-[#020617] min-h-screen text-white" dir="rtl">
+    <div className="bg-[#020617] min-h-screen text-white">
       <Navbar />
       <Hero />
       <Features />
       <AITutor />
-      <ContactBar />
     </div>
   );
 }

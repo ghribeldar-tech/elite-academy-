383b4d74ddaa709b09230c77f68408ec27af42c7
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-17 03:25:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  define: { 'process.env.API_KEY': JSON.stringify(process.env.API_KEY) }
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+})

39a11b0def15869bbcc29705ee48855289797226
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-19 09:05:45 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Navbar, Hero, Features, AITutor, SourceExplorer } from './components';
+// استدعاء المكونات بشكل مباشر لضمان نجاح الرفع على Vercel
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero';
+import { Features } from './components/Features';
+import { CourseLevels } from './components/CourseLevels';
+import { AITutor } from './components/AITutor';
+import { SocialMediaKit } from './components/SocialMediaKit';
+import { AdPromotion } from './components/AdPromotion';
+import { ContactBar } from './components/ContactBar';
+import { SourceExplorer } from './components/SourceExplorer';
 
-const App = () => (
-  <div className="bg-[#020617] text-white min-h-screen" dir="rtl">
-    <Navbar />
-    <Hero />
-    <div className="max-w-7xl mx-auto px-6 py-20" id="tutor">
-      <AITutor />
+const App = () => {
+  return (
+    <div className="bg-[#020617] text-white min-h-screen" dir="rtl">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <CourseLevels />
+        {/* قسم المدرس الذكي */}
+        <div id="tutor">
+          <AITutor />
+        </div>
+        <SocialMediaKit />
+        <AdPromotion />
+      </main>
+      <ContactBar />
+      <SourceExplorer />
     </div>
-    <SourceExplorer />
-  </div>
-);
+  );
+};
+
 export default App;

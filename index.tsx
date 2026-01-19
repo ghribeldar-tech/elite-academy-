1f6871cde9d0bb116b176038f0c1cf9758b124b3
ghribeldar-tech <ghribeldar@gmail.com>
2026-01-19 08:39:32 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './style.css'; // استدعاء ملف الـ CSS بما أنه موجود في نفس المجلد
 
 const container = document.getElementById('root');
+
 if (container) {
   const root = createRoot(container);
-  root.render(<React.StrictMode><App /></React.StrictMode>);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
